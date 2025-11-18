6de2052c1a8ffa89f05c44eb138f08040d7428f3
Nandan81979 <nandanshettar8197@gmail.com>
2025-11-18 20:51:03 +0530

    Update pali.py

--- pali.py
+++ pali.py
@@ -1,27 +1,12 @@
 import sys
-
-
-if len(sys.argv) > 1:
-    script_name=sys.argv[0]
-    scores = sys.argv[1:]
-    
-    scores = [float(x) for x in scores]
+if len(sys.argv) == 2:
+    script_name = sys.argv[0]
+    string = sys.argv[1]
 else:
-   
-    scores = [50, 60, 70, 80, 90]
-    print("No input given, using default scores:", scores)
-
-
-total = sum(scores)
-average = total / len(scores)
-
-
-print("Scores:", scores)
-print("Sum of Scores:", total)
-print("Average of Scores:", average)
-
-max=max(scores)
-min=min(scores)
-
-print("Minimum : ",min)
-print("Maximum : ",max)
+    script_name = sys.argv[0]
+    string = "madam"
+print("Invalid input using default values")
+if string == string[::-1]:
+    print(string, "is a palindrome")
+else:
+    print(string, "is not a palindrome")
